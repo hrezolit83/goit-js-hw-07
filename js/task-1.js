56763a574702948b2961604d8418d8affdaec786
hrezolit83 <deduh83@gmail.com>
2024-01-14 02:42:59 +0200

    start3

---
+++ js/task-1.js
@@ -0,0 +1,10 @@
+"use strict";
+const categoriesCount = document.querySelectorAll("li.item");
+console.log(`Number of categories: ${categoriesCount.length}`);
+
+categoriesCount.forEach((category) => {
+    const headingText = category.querySelector("h2").textContent;
+    const itemsCount = category.querySelectorAll("li").length;
+    console.log(`Category: ${headingText}`);
+    console.log(`Elements: ${itemsCount}`);
+});
